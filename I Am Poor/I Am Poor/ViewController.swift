e69578bba63b5ff0d3f70a5679b1ad62a9c90880
Gagan <g.deepsingh1@gmail.com>
2020-07-20 02:39:29 +0530

    initial projects

---
+++ I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Gagan on 19/07/20.
+//  Copyright © 2020 Gagan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Gagan on 16/07/20.
+//  Copyright © 2020 Gagan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

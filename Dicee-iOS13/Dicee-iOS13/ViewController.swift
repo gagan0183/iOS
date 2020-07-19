6a4eb6630eca71b34f021e49ebd0cb6b6457e6bd
Gagan <g.deepsingh1@gmail.com>
2020-07-20 02:40:44 +0530

    alpha to image

--- Dicee-iOS13/Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/Dicee-iOS13/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
+        diceImageView1.alpha = 0.5
     }
 
 

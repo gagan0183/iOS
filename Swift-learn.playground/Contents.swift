1142c90d4f5962c058db9fd9307748801a09d128
Gagan <g.deepsingh1@gmail.com>
2021-03-04 12:04:10 +0530

    comments

--- Swift-learn.playground/Contents.swift
+++ Swift-learn.playground/Contents.swift
@@ -1,19 +1,29 @@
 import UIKit
 
+// day 1
+
+// variables
 var str = "Hello, playground"
+
+// constants
+let s = "this"
 
 var value = 1
 
+// multi line strings
 var multiline = """
 1
 2
 3
 """
 
+// string interpolations
 var score = 10
 var stringInterpolate = "The score is \(score)"
 
-
+// type annotations
 var values: Int = 10
 
 var name: String
+
+// day 2

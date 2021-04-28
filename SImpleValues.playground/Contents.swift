e7d9c99b6aa6153e0c85234baae62c0df5a4b21a
Gagan <g.deepsingh1@gmail.com>
2021-04-28 21:32:54 +0530

    variables and constants

--- SImpleValues.playground/Contents.swift
+++ SImpleValues.playground/Contents.swift
@@ -2,4 +2,29 @@ import UIKit
 
 // Simple values
 
+42
+-23
+3.141591
+0.1
+-273
+true
+false
+"hello"
+"albatross"
+
+// constants and variables
+
+let theAnswerToTheUtimateQuestion = 100
+let pi = 3.141
+let myName = "Gagan"
+
+var currentTemperatureInCelsius = 98.3
+var myAge = 34
+var myLocation = "Canada"
+
+var isRaining = true
+isRaining = false
+
+
+
 
